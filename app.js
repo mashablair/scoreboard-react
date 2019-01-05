5a833a059cf03fc6fedce802984542b7bf24ffa7
Masha Blair <mashablair@gmail.com>
2019-01-04 16:07:43 -0800

    First React component using function declaration.

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const title = 'My First React Element!';
-const desc = 'I just learned how to create a React node and render it into the DOM';
-
-const header = (
-	<header>
-		<h1 className="main-title">{ title }</h1>
-		<p>{ desc }</p> 
-	</header>
-);
+function Header() {
+	return (
+		<header>
+			<h1>Scoreboard</h1>
+			<span className="stats">Players: 1</span>
+		</header>
+	)
+}
 
 ReactDOM.render(
-	header,
+	<Header />,
 	document.getElementById('root')
 );
-
-console.log(title);
-console.log(desc);
-console.log(header);
