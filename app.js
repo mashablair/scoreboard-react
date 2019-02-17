b3c5712842f5e21401f601e952cc0671e827e5f8
Masha Blair <mashablair@gmail.com>
2019-02-17 14:12:45 -0800

    Add simple incrementScore method inside Counter component and call it from onClick event handler on increment button.

--- app.js
+++ app.js
@@ -34,28 +34,22 @@ const Header = (props) => {
 	);
 }
 
-//const Counter = (props) => {
-//	return (
-//		<div className="counter">
-//			<button className="counter-action decrement"> - </button>
-//			<span className="counter-score">{props.score}</span>
-//			<button className="counter-action increment"> + </button>
-//		</div>
-//	);
-//}
-
 class Counter extends React.Component {
 	
 	state = {
 		score: 0
 	};
+
+	incrementScore() {
+		console.log('from incrementScore')
+	}
 	
 	render() {
 		return (
 			<div className="counter">
 				<button className="counter-action decrement"> - </button>
 				<span className="counter-score">{this.state.score}</span>
-				<button className="counter-action increment"> + </button>
+				<button className="counter-action increment" onClick={this.incrementScore}> + </button>
 			</div>
 		);	
 	}
